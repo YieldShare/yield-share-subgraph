d6b1f2c8571c45ee21fd7236142ef484a178a9da
agusduha <agusnduha@gmail.com>
2023-08-08 16:13:54 -0300

    feat: add data source template

--- src/yield-share-factory.ts
+++ src/yield-share-factory.ts
@@ -1,42 +1,43 @@
 import {
   OwnershipTransferred as OwnershipTransferredEvent,
   TreasuryChanged as TreasuryChangedEvent,
-  YieldShareVaultCreated as YieldShareVaultCreatedEvent
-} from "../generated/YieldShareFactory/YieldShareFactory"
+  YieldShareVaultCreated as YieldShareVaultCreatedEvent,
+} from "../generated/YieldShareFactory/YieldShareFactory";
 import {
   OwnershipTransferred,
   TreasuryChanged,
-  YieldShareVaultCreated
-} from "../generated/schema"
+  YieldShareVaultCreated,
+} from "../generated/schema";
+import { YieldShare } from "../generated/templates";
 
 export function handleOwnershipTransferred(
   event: OwnershipTransferredEvent
 ): void {
   let entity = new OwnershipTransferred(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.user = event.params.user
-  entity.newOwner = event.params.newOwner
+  );
+  entity.user = event.params.user;
+  entity.newOwner = event.params.newOwner;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleTreasuryChanged(event: TreasuryChangedEvent): void {
   let entity = new TreasuryChanged(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.oldTreasury = event.params.oldTreasury
-  entity.newTreasury = event.params.newTreasury
+  );
+  entity.oldTreasury = event.params.oldTreasury;
+  entity.newTreasury = event.params.newTreasury;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleYieldShareVaultCreated(
@@ -44,13 +45,15 @@ export function handleYieldShareVaultCreated(
 ): void {
   let entity = new YieldShareVaultCreated(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.vault = event.params.vault
-  entity.yieldShare = event.params.yieldShare
+  );
+  entity.vault = event.params.vault;
+  entity.yieldShare = event.params.yieldShare;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
+
+  YieldShare.create(event.params.yieldShare);
 }
--- src/yield-share.ts
+++ src/yield-share.ts
@@ -3,42 +3,42 @@ import {
   SharesWithdrawn as SharesWithdrawnEvent,
   YieldSharingCollected as YieldSharingCollectedEvent,
   YieldSharingStarted as YieldSharingStartedEvent,
-  YieldSharingStopped as YieldSharingStoppedEvent
-} from "../generated/YieldShare/YieldShare"
+  YieldSharingStopped as YieldSharingStoppedEvent,
+} from "../generated/YieldShare/YieldShare";
 import {
   SharesDeposited,
   SharesWithdrawn,
   YieldSharingCollected,
   YieldSharingStarted,
-  YieldSharingStopped
-} from "../generated/schema"
+  YieldSharingStopped,
+} from "../generated/schema";
 
 export function handleSharesDeposited(event: SharesDepositedEvent): void {
   let entity = new SharesDeposited(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.user = event.params.user
-  entity.shares = event.params.shares
+  );
+  entity.user = event.params.user;
+  entity.shares = event.params.shares;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleSharesWithdrawn(event: SharesWithdrawnEvent): void {
   let entity = new SharesWithdrawn(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.user = event.params.user
-  entity.shares = event.params.shares
+  );
+  entity.user = event.params.user;
+  entity.shares = event.params.shares;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleYieldSharingCollected(
@@ -46,18 +46,18 @@ export function handleYieldSharingCollected(
 ): void {
   let entity = new YieldSharingCollected(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.senderBalance = event.params.senderBalance
-  entity.receiverBalance = event.params.receiverBalance
-  entity.feeBalance = event.params.feeBalance
+  );
+  entity.from = event.params.from;
+  entity.to = event.params.to;
+  entity.senderBalance = event.params.senderBalance;
+  entity.receiverBalance = event.params.receiverBalance;
+  entity.feeBalance = event.params.feeBalance;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleYieldSharingStarted(
@@ -65,18 +65,18 @@ export function handleYieldSharingStarted(
 ): void {
   let entity = new YieldSharingStarted(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.shares = event.params.shares
-  entity.assets = event.params.assets
-  entity.percentage = event.params.percentage
+  );
+  entity.from = event.params.from;
+  entity.to = event.params.to;
+  entity.shares = event.params.shares;
+  entity.assets = event.params.assets;
+  entity.percentage = event.params.percentage;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleYieldSharingStopped(
@@ -84,16 +84,16 @@ export function handleYieldSharingStopped(
 ): void {
   let entity = new YieldSharingStopped(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.senderBalance = event.params.senderBalance
-  entity.receiverBalance = event.params.receiverBalance
-  entity.feeBalance = event.params.feeBalance
+  );
+  entity.from = event.params.from;
+  entity.to = event.params.to;
+  entity.senderBalance = event.params.senderBalance;
+  entity.receiverBalance = event.params.receiverBalance;
+  entity.feeBalance = event.params.feeBalance;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
